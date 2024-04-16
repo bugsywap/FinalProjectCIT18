5bf763529b158109bfd0095622ac8e1d01267b1d
echotamondong <shinoecho@gmail.com>
2024-04-16 22:47:53 +0800

    footer.blade.php design implemented

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,2 +1,6 @@
-</body>
-</html>
+<footer class="fixed bottom-0 left-0 right-0 bg-blue-600 text-white text-center py-4">
+    <div class="container mx-auto">
+        <p class="italic m-0"> ✨"The best startups generally come from somebody needing to scratch an itch." - Michael Arrington</p>
+        <p class="text-blue-900 font-bold text-xs m-0">-- A CIT18 Final Project by Gabriel Albert P. Ollero, Jericho D. Tamondong, & Leslie Faye G. Fuentes --</p>
+    </div>
+</footer>
